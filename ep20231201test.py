85e4e447115d797f5fd9fe2e0350a1a56c994d5a
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2023-12-12 09:32:31 +0500

    Ковырял, как выводится и сохраняется тип

--- ep20231201test.py
+++ ep20231201test.py
@@ -1,3 +1,7 @@
 
 if __name__ == '__main__':
-    print('Ghbdtn Привет')
+    DB_NAME = "wwwqqq.exe"
+    DB_NAME_RW = "file:" + DB_NAME + "?mode=rw"
+    print(type(DB_NAME_RW))
+    print(DB_NAME_RW)
+    print(str(DB_NAME_RW))
