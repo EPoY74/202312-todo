692c5b6dc6a5ca58a86838a2724b676562f0d46f
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-01-18 22:17:49 +0500

    А декоратор заработал! Ура, товарищи!

--- spam_files/ep_20240117_test.py
+++ spam_files/ep_20240117_test.py
@@ -1,5 +1,13 @@
 import requests as req
 
+def outer(foo):
+    def inner(*args, **kwargs):
+        print("Start")
+        foo(*args, **kwargs)
+        print("end")
+    return inner
+
+@outer
 def load_web_page(site_name: str):
     """
     Автор: Евгений Петров
@@ -7,7 +15,10 @@ def load_web_page(site_name: str):
     Цель: Проверить скорость загрузки страницы или что-то еще
           для проверки работы работы декоратора 
     """
-
+    
     print(req.get(site_name))
+
+
+
 
 load_web_page("http://ya.ru")
