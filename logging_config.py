a81b175387109221e1d48d1b1410e73b71b14040
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-01-31 00:17:49 +0500

    Настроил конфигурацию логгера для вывода в файл, а не на экран

--- logging_config.py
+++ logging_config.py
@@ -1,6 +1,8 @@
 import logging
 
 # Конфигурирование логгера
+
 FORMAT = '[%(levelname)s] %(asctime)s - %(message)s'
-logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+# logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG, filename="ep20231204_todo_cli.log",format=FORMAT,)
 logger = logging.getLogger('todologger')
