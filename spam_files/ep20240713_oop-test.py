223173910a27f988af2764de41315cb140689d60
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-07-14 08:45:07 +0500

    Урок по ООП закончен

--- spam_files/ep20240713_oop-test.py
+++ spam_files/ep20240713_oop-test.py
@@ -15,7 +15,7 @@
 # Создаю класс Purse
 class Purse:
 
-    def __init__(self, valuta="RUR", name="Unknown"):
+    def __init__(self, valuta, name="Unknown"):
         """
         Это конструктор объекта
         Код внутри этого метода выполняется тогда, когда создается экземпляр класса
@@ -26,6 +26,10 @@ class Purse:
         """
 
         # Так как есть self, то это свойство конкретного экземпляра
+        if valuta not in ("EUR", "USD"):
+            # Исключение, что бы ошибку можно было обработать
+            raise ValueError("Не соответствие валюты. Можно только EUR или USD")
+
         self.__money = 0.00
         self.valuta = valuta
         self.name = name
@@ -33,13 +37,15 @@ class Purse:
     def top_up_balance(self, howmany):
         # Пополнение кошелька, на сколько мы его пополнили. Пополнение суммируется с доступным остатком
         self.__money = self.__money + howmany
+        return howmany # возвращаю значение, что бы можно было осуществить перевод (тут до кучи)
 
     def top_down_balance(self, howmanydown):
         # Расходы с кошелька - сколько потратили. Уменьшает доступный остаток.
-        if self.money < howmanydown:
+        if self.__money < howmanydown:
             print("Недостаточно средств. пополните, пожалуйста, кошелек")
             raise ValueError("Недостаточно средств")  # Исключение, что бы ошибку можно было обработать
         self.__money = self.__money - howmanydown
+        return howmanydown  # возвращаю значение, что бы можно было осуществить перевод
 
     def info(self):
         # Возвращает количество денег в конкретном кошельке
@@ -48,11 +54,11 @@ class Purse:
     def __del__(self):
         # Деструктор, выполняется во время удаления, всегда
         print("Кошелек удален")
-        return self.___money
+        return self.__money
 
 
 x = Purse("USD")
-y = Purse("GBP", "Ann")
+y = Purse("EUR", "Ann")
 print(x.info())
 x.money = 150
 print(x.info())
@@ -61,5 +67,12 @@ x.top_up_balance(300)
 print(x.info())
 x.top_down_balance(100)
 print(x.info())
-x.top_down_balance(351.1)
+# x.top_down_balance(351.1)
+# print(x.info())
+x.__money = -300
+print(x.info())
+y.top_up_balance(20)
+print(y.info())
+x.top_up_balance(y.top_down_balance(10))
+print(y.info())
 print(x.info())
