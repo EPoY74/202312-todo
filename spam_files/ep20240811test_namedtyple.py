42323cc3aa98975f673f68e192317200ad3f8e3d
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-08-11 11:31:56 +0500

    Поправил предупреждения

--- spam_files/ep20240811test_namedtyple.py
+++ spam_files/ep20240811test_namedtyple.py
@@ -1,28 +1,46 @@
 """
-программа изучает использование namedtyple, и отвязывании
+Программа изучает использование namedtyple, и отвязывании
 данных от поведения.
-Источник: ЗАкрытая группа тестирования УЧЕБНИКА pYTHON, ГРирорий Петров
+Источник: Закрытая группа тестирования учебника Python, Григорий Петров
 Автор: Евгений Петров
 Почта: p174@mail.ru
 ДАта: 20240811
 """
 import collections
-import typing
 
-Teacher = collections.namedtuple('Teacher', {'name': str , 'subject': str})
-Student = collections.namedtuple('Student', {'name': str})
-type = SchoolPerson = Teacher | Student
-
-
-def get_name(obj: SchoolPerson) -> str:
-    return obj.name
+Teacher = collections.namedtuple('Teacher',
+                                 ['name', 'subject'])
+Student = collections.namedtuple('Student', ['name'])
+type  SchoolPerson = Teacher | Student
 
 
-teacher = Teacher("Eugernii Petrov", "Programming")
+def get_name(obj_name: SchoolPerson) -> str:
+    """
+    Выводит только имя из передаваемого объекта
+    """
+    return obj_name.name
+
+
+def get_subject(obj_sub: SchoolPerson):
+    """
+    Выводит только преподаваемый предмет
+    """
+    try:
+        return obj_sub.subject
+    except AttributeError as err:
+        if err:
+            pass
+        return None
+
+
+teacher = Teacher("Eugenii Petrov", "Programming")
 student = Student("Elina Shefer")
 
 print(student.name)
 print(teacher.name)
 
 print(get_name(student))
+print(get_subject(student))
+print(20 * "-")
 print(get_name(teacher))
+print(get_subject(teacher))
