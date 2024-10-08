0fe46f680c97e5e835227e34d6de4158ae0ac113
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-08 06:31:44 +0500

    Тыкаю палкой sqlite, смотрел, есть ли в ответе имена полей

--- ep20241002_todo_api.py
+++ ep20241002_todo_api.py
@@ -6,17 +6,28 @@
 Дата начала: 02.10.2024
 """
 
+from datetime import datetime
 from fastapi import FastAPI
+import colorama 
 
 from cfg_working import search_config_and_db
 import db_working_api
 
 from logging_cfg import logger
 
+colorama.init()
 app_todo = FastAPI()
 
-print("""\nAPI: Консольное приложение для ведения задач.
-        \nАвтор: Евгений Б. Петров, p174@mail.ru\n""")
+# Получаем объект дата время
+date_time_now_obj = datetime.now()
+
+# Преобразовываем его как нам надо
+date_time_now = date_time_now_obj.strftime('%d.%m.%Y %H:%M')
+date_time_now_new = datetime.now().strftime('%d.%m.%Y %H:%M')
+
+print(colorama.Fore.YELLOW + "\nЗапeщено в " + date_time_now)
+print("API: Консольное приложение для ведения задач.")
+print("Автор: Евгений Б. Петров, p174@mail.ru\n")
 
 
 # Получаю имя базы данных из файла с конфигурацией
@@ -35,6 +46,7 @@ async def root_async():
 
     return {"mеssage" : "This is API for EPoY's todo app. Since 2023."}
 
+
 @app_todo.get("/all")
 async def show_all_tasks_async():
     """Выводит все записи, которые есть в БД
@@ -42,6 +54,7 @@ async def show_all_tasks_async():
     logger.info("API: Обращение с API по роуту '/all'")
 
     return db_working_api.list_of_tasks_json(db_name)
+
 
 @app_todo.get("/last")
 async def show_last_tasks_async():
