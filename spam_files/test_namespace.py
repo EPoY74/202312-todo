040d0c7c7b376edb0ee251f80e9a3b7f439ae402
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-11-27 21:28:18 +0500

    Проверка Класса без экземпара класса

---
+++ spam_files/test_namespace.py
@@ -0,0 +1,8 @@
+class Text:
+    def as_web_safe(text): print(text)
+    def is_web_safe(text): pass
+    def without_spaces(text): pass
+    
+if __name__ == "__main__":
+    Text.as_web_safe("Привет")
+    
