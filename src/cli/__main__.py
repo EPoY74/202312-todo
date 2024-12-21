67b697137fbac609b3c25927daa8ad3f5a22042e
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-12-21 14:23:15 +0500

    Сделал запуск модуля, как кода (название модуля - и все)

--- src/cli/__main__.py
+++ src/cli/__main__.py
@@ -2,12 +2,13 @@
 """
 import sys
 import os
+from src.cli.cli import main
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-
-
-
-from .cli import main
+if not __package__:
+    #  Сделать CLI запускаемым из исходного дерева с помощью
+    # python src/pakage
+    package_source_path = os.path.dirname(os.path.dirname(__file__))
+    sys.path.insert(0, package_source_path)
 
 if __name__ == "__main__":
     main()
