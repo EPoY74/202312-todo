70fda356a06ba53937decc85b45981831bcdd2ae
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-12-21 16:05:27 +0500

    Поправил пути к файлу, поправил кодировку логирования, указав явно utf-8

--- src/cfg/logger_config.py
+++ src/cfg/logger_config.py
@@ -7,9 +7,25 @@
 """
 
 import logging
+import os
+
+file_directory:str = os.path.dirname(__file__)
+logger_file_path:str = os.path.join(
+    file_directory,
+    '..',
+    'log', 'todo_cli.log'
+)
+print("logger_config: ", logger_file_path)
 
 
 # Конфигурирование логгера
 FORMAT = '[%(levelname)s] %(asctime)s - %(message)s'
-logging.basicConfig(format=FORMAT, level=logging.DEBUG,  filename="todo_cli.log",filemode="a")
+logging.basicConfig(
+    format=FORMAT,
+    level=logging.DEBUG,
+    filename=logger_file_path,
+    filemode="a",
+    encoding="utf-8"
+    )
 logger = logging.getLogger('todologger')
+logger.debug("Это тестовое сообщение для проверки логгера.")
