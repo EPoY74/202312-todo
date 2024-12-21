55a3b889c2b5e8407e05f4366fb83d060abd0d16
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-12-21 16:04:42 +0500

    Поправил для корректного запуска модуля, как скрипта

--- src/api/__main__.py
+++ src/api/__main__.py
@@ -1,13 +1,21 @@
 """Для запуска api  приложения
 """
 
+import os
+import sys
 import uvicorn
-from main_api import app_todo
+# from src.api.main_api import app_todo
+
+if not __package__:
+    #  Сделать CLI запускаемым из исходного дерева с помощью
+    # python src/pakage
+    package_source_path = os.path.dirname(os.path.dirname(__file__))
+    sys.path.insert(0, package_source_path)
 
 def main():
     """пробую запускать api приложение
     """
-    uvicorn.run(app_todo, reload=True)
+    uvicorn.run("src.api.main_api:app_todo", host="127.0.0.1", port=8000, reload=True)
 
 if __name__ == "__main__":
     main()
