a85d68276256ed1e4fe58a93a9a2186595370fe5
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-12-29 10:04:01 +0500

    Трогаю лапкой редис

---
+++ src/reddis/redis_hash._example.py
@@ -0,0 +1,12 @@
+import redis
+
+# Подключение к Redis
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+# Установка значений в хэше
+myhash:str = "myhash"
+r.hset(myhash, {'field1': 'value1', 'field2': 'value2'})
+
+# Получение значений из хэша
+hash_values = r.hgetall(myhash)
+print(hash_values)
