af13a66c9bcface28768c1f5f974ca2de203bf36
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-20 07:17:31 +0500

    Фиксирую изменения сделанные и закомментированные чуть ранее.

--- src/cli/!__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-if not __package__:
-    #  Сделать CLI запускаемым из исходного дерева с помощью
-    # python src/pakage
-    package_source_path = os.path.dirname(os.path.dirname(__file__))
-    sys.path.insert(0, package_source_path)
-    print(sys.path)
