c5d4955a6c775b3181a96ead14ff98ce81396867
Evgenii Petrov Linux IceBook <epoy74@gmail.com>
2025-11-02 22:57:42 +0500

    refactor(cfg_working) Improve variables names

--- src/cfg/cfg_working.py
+++ src/cfg/cfg_working.py
@@ -25,39 +25,39 @@ def search_config_and_db(name_exicting_db: str = "" ):
     prog_name: str = "todo_main"
 
     # Формирую имя файла конфигурации
-    inner_for_search_ini_file_name = "todo_config.ini"
+    ini_config_name = "todo_config.ini"
     file_directory:str = os.path.dirname(__file__)
     config_file_path:str = os.path.join(
         file_directory,
         '..', '..',
-        'src', 'cfg', inner_for_search_ini_file_name
+        'src', 'cfg', ini_config_name
     )
     print("cfg_working: ", config_file_path)
 
 
     # Формирую имя БД
-    inner_for_search_db_file_name = str(prog_name + ".db")
-    db_file_name_path:str = os.path.join(
+    db_name = str(prog_name + ".db")
+    db_path:str = os.path.join(
         file_directory,
         '..', '..',
-        'src', 'data', inner_for_search_db_file_name
+        'src', 'data', db_name
     )
-    print("cfg_working: ",  db_file_name_path)
+    print("cfg_working: ",  db_path)
 
-    inner_for_search_todo_config_obj = cfg_par.ConfigParser()
+    config_obj = cfg_par.ConfigParser()
 
     # задаю объект парсера конфигурации
     # Читаю конфигурацию
-    todo_config = inner_for_search_todo_config_obj.read(
+    todo_config = config_obj.read(
         config_file_path
         )
 
     if len(todo_config) == 0:
-        if not os.path.isfile(db_file_name_path):
+        if not os.path.isfile(db_path):
             print("БД не создана")
-        is_confirm = input(f"Создать базу данных {inner_for_search_db_file_name}? y/n ")
+        is_confirm = input(f"Создать базу данных {db_name}? y/n ")
         if is_confirm.upper() == "Y":
-            make_db(db_file_name_path)
+            make_db(db_path)
 
         elif is_confirm.upper() == "N":
             print("Отменяю создание базы данных")
@@ -66,35 +66,35 @@ def search_config_and_db(name_exicting_db: str = "" ):
             print('Вы ввели не корректное значение. Введите "y" или "n"!')
             exit(1)
 
-        print(f"\nфайл конфигурации {inner_for_search_ini_file_name} не найден")
-        is_confirm = input(f"Создать файл конфигурации {inner_for_search_ini_file_name}? y/n ")
+        print(f"\nфайл конфигурации {ini_config_name} не найден")
+        is_confirm = input(f"Создать файл конфигурации {ini_config_name}? y/n ")
         if is_confirm.upper() == "y" or is_confirm == "Y":
             create_config_file(
                 config_file_path,
-                inner_for_search_db_file_name)
+                db_name)
         elif is_confirm.upper() == "n":
             print("Отменяю создание файла конфигурации")
             exit(1)
         else:
             print('Вы ввели не корректное значение. Введите "y" или "n"!')
             exit(1)
-    return inner_for_search_todo_config_obj
+    return config_obj
 
 
 
 def create_config_file(ini_file_name: str,
-                       db_name_for_create_config: str):  # Создаю файл конфигурации
+                       db_name: str):  # Создаю файл конфигурации
     """Создает файл конфигурации
 
     Args:
         ini_file_name (str): Имя файла конфигурации??? Сам забыл уже
-        db_name_for_create_config (str): Имя базы данных???? Сам забыл уже
+        db_name (str): Имя базы данных
     """
 
     print("\n\nСоздаю файл конфигурации...")
     todo_config = cfg_par.ConfigParser()
     todo_config.add_section("db_cfg")
-    todo_config.set("db_cfg", "db_name", db_name_for_create_config)
+    todo_config.set("db_cfg", "db_name", db_name)
 
     with open(ini_file_name, "w", encoding="utf-8") as cfg_file:
         todo_config.write(cfg_file)
