8c57f1c931106108a081673e48bda205aeae54a2
Evgenii Petrov IceBook <epoy74@gmail.com>
2025-12-06 13:26:55 +0500

    fix(logger): remove print, log test message only when creating log file

--- src/cfg/logger_config.py
+++ src/cfg/logger_config.py
@@ -13,10 +13,12 @@ LOG_FILENAME: str = "todo_cli.log"
 file_directory: str = os.path.dirname(__file__)
 logger_dir: str = os.path.join(file_directory, "..", "log")
 logger_file_path: str = os.path.join(logger_dir, LOG_FILENAME)
-print("logger_config: ", logger_file_path)
+# Устанавливаем переменнную, для проверки новизны файла
+is_file_new = False
 os.makedirs(logger_dir, exist_ok=True)
 if not os.path.exists(logger_file_path):
     open(logger_file_path, "a").close()
+    is_file_new = True
 
 # Конфигурирование логгера
 FORMAT = "[%(levelname)s] %(asctime)s - %(message)s"
@@ -28,4 +30,5 @@ logging.basicConfig(
     encoding="utf-8",
 )
 logger = logging.getLogger("todologger")
-logger.debug("Это тестовое сообщение для проверки логгера.")
+if is_file_new:
+    logger.debug("Это тестовое сообщение для проверки логгера.")
