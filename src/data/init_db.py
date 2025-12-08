a2ee7b3f3fc2cf1eaea8106a9eb6538b5a6e75fe
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2025-12-08 13:41:56 +0500

    chore(db): clean up and finalize init_db script

--- src/data/init_db.py
+++ src/data/init_db.py
@@ -1,15 +1,23 @@
+import os
 import sqlite3
-from pathlib import Path
 
-DB_PATH = Path("todo_main.db")
-DUMP_PATH = Path("sqlite-example-data-db-utf-8.sql")
+DB_NAME: str = "todo_main.db"
+DUMP_NAME: str = "sqlite-example-data-db-utf-8.sql"
+FOLDER_PATH: str = os.path.dirname(__file__)
+
+DB_PATH: str = os.path.join(FOLDER_PATH, DB_NAME)
+DUMP_PATH: str = os.path.join(FOLDER_PATH, DUMP_NAME)
+
 
 def init_db() -> None:
-    DB_PATH.parent.mkdir(parents=True, exist_ok=True)
-    sql = DUMP_PATH.read_text(encoding="utf-8")
+
+    with open(DUMP_PATH, encoding="utf-8") as dump_sql_file:
+        sql_dump = dump_sql_file.read()
+    print("SQL dump file has been read successfully.")  #noqa T201
 
     with sqlite3.connect(DB_PATH) as conn:
-        conn.executescript(sql)
+        conn.executescript(sql_dump)
+
 
 if __name__ == "__main__":
     init_db()
